1edbc8f180efc3f4d1b1dbb2270e12f7bb4ac381
Michal t <102900827+Mttt7@users.noreply.github.com>
2022-08-26 17:08:08 +0000

    x

--- index.js
+++ index.js
@@ -1,59 +1,79 @@
 /* 97-122 */
-let letters=[]
-for(let i=97;i<=122;i++){
-  if(String.fromCharCode(i).match(/[a-z]/i)){
+/* 97-122 */
+let letters = []
+for (let i = 97; i <= 122; i++) {
+  if (String.fromCharCode(i).match(/[a-z]/i)) {
     let char = String.fromCharCode(i)
     let index = i
-    console.log(char,':',index)
-    let letter= new Letter(char,index)
+    
+    let letter = new Letter(char, index)
     letters.push(letter)
 
   }
 }
-function Letter(char,index){
+function Letter(char, index) {
   this.char = char
-  this.index=index 
+  this.index = index
 }
 
-function shifted(letter,shift){
-    newIndex=getIndex(letter)+shift
-    if(newIndex<97){
-      let gap = 97-newIndex
-      newIndex = 122-gap
-    }
-    return getLetter(newIndex)
+function shifted(letter, shift) {
+  let isCapitalized
+  if(letter==letter.toUpperCase()) isCapitalized=true
+  if(letter==letter.toLowerCase()) isCapitalized=false
+  newIndex = getIndex(letter.toLowerCase()) + shift
+  if (newIndex < 97) {
+    let gap = 97 - newIndex-1
+    newIndex = 122 - gap
   }
-  
-  function getLetter(ind){
-    for(let i=0;i<122-97;i++){
-      if(ind==letters[i].index) return letters[i].char
+  if(newIndex>122){
+    let gap = newIndex - 122-1
+    newIndex = 97 + gap
+  }
+  if(isCapitalized) return getLetter(newIndex).toUpperCase()
+  if(!isCapitalized) return getLetter(newIndex)
+}
+
+function getLetter(ind) {
+  let result
+  for (let i = 0; i <=122 - 97; i++) {
+    if (ind == letters[i].index) result = letters[i].char
+  }
+  return result
+}
+
+function getIndex(lett) {
+  for (let i = 0; i <=122 - 97; i++) {
+    if (lett == letters[i].char) {
+      
+      return letters[i].index
     }
   }
+}
 
-  function getIndex(lett){
-    for(let i=0;i<122-97;i++){
-      if(lett==letters[i].char){
-        console.log("XX:",letters[i].index)
-        return letters[i].index
-      } 
+
+
+
+
+const caesar = function(word, shift) {
+  let strAfter = ''
+  for (let i = 0; i < word.length; i++) {
+    if (word[i].match(/[a-z]/i)) {
+      strAfter += shifted(word[i], shift)
     }
+    else{
+      strAfter +=word[i]
+    }
+
   }
+  return strAfter
+};
 
 
 
 
 
-const caesar = function(word,shift) {
-    let strAfter=''
-      for(let i=0;i<word.length;i++){
-          if(word[i].match(/[a-z]/i)){
-            strAfter+=shifted(word[i],shift)
-          }
-          
-      }
-    return strAfter
-  };
+
+// Do not edit below this line
 
 
-  console.log(caesar('Mjqqt, Btwqi!', -5))
-
+console.log(caesar('Siemka',75))
